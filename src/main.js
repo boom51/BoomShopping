dc89da0c555f6a846aa809c698c0c1ffcd77e4da
boom51 <boom9612@outlook.com>
2019-03-14 15:54:00 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
 import Vue from 'vue'
 import app from './app.vue'
-import {Header} from 'mint-ui'
+import {Header,Swipe,SwipeItem} from 'mint-ui'
+
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+Vue.use(VueRouter)
 Vue.component(Header.name,Header)
+Vue.component(Swipe.name,Swipe)
+Vue.component(SwipeItem.name,SwipeItem)
+//导入router.js模块
+import router from './router.js'
 var vm=new Vue(
     {
         el:'#app',
-        render:c=>c(app)
+        render:c=>c(app),
+        router
     }
 )
--- src/router.js
+++ src/router.js
@@ -1,7 +1,17 @@
 import VueRouter from 'vue-router'
+import HomeContainer from './components/tabbar/HomeContainer.vue'
+import MemberContainer from './components/tabbar/MemberContainer.vue'
+import SearchContainer from './components/tabbar/SearchContainer.vue'
+import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
 var router =new VueRouter({
-    routes:[
-
-    ]
+    routes:[//配置路由规则
+        {path:'/',component:HomeContainer},
+        {path:'/home',component:HomeContainer},
+        {path:'/member',component:MemberContainer},
+        {path:'/search',component:SearchContainer},
+        {path:'/shopcar',component:ShopcarContainer},
+    ],
+    //路由高亮
+    linkActiveClass:'mui-active'//覆盖默认的路由高亮的类
 })
 export default router
